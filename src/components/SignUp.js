2e7c70552d4952b0c0e57e7ea7351d4738e2c073
Mustafa Ata Onbas <aonbas@sabanciuniv.edu>
2022-10-27 15:39:37 +0300

    fixes on page functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,18 @@
 import './SignUp.css'
 import {Form, InputGroup, Button} from 'react-bootstrap';
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import Axios from 'axios';
 
 function SignUp() {
-  
+
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-  const [userList, setUserList] = useState([]); //here adding feature direct adding to list of map. 4-5 minutes
+  // const [userList, setUserList] = useState([]); //here adding feature direct adding to list of map. 4-5 minutes
+  // const [newName, setNewName] = useState("");
 
-  const [newName, setNewName] = useState("")
   // useEffect(() => { //burası get ile db'den alınacaklar için kullanılıyor. useEffect !!
   //   Axios.get("http://localhost:3001/api/get").then((response) => {
   //     console.log(response.data);
@@ -32,20 +32,42 @@ function SignUp() {
     });
   };
 
-  const deleteUser = (user) => {
-    Axios.delete('http://localhost:3001/api/delete/${user}' )
-  }
+  // const deleteUser = (user) => {
+  //   Axios.delete('http://localhost:3001/api/delete/${user}' )
+  // }
 
-  const updateUser = (username) => {
-    Axios.put("http://localhost:3001/api/update", {
-      name: name,  //BURALAR KARIŞTI
-      surname: surname,
-      email: email,
-      password: password,
-      username: username
-    });
-    setNewName("")
-  }
+  // const updateUser = (username) => {
+  //   Axios.put("http://localhost:3001/api/update", {
+  //     name: name,  //BURALAR KARIŞTI
+  //     surname: surname,
+  //     email: email,
+  //     password: password,
+  //     username: username
+  //   });
+  //   setNewName("")
+  // }
+
+  // {userList.map((val) => {
+  //   return (
+  //     <div className='card'>
+  //       <h1>{val.email}</h1>
+  //       <p>{val.password}</p>
+
+  //       <button onClick={() => {deleteUser(val.username);
+  //       }}
+  //       >
+  //         Delete
+  //         </button>
+  //         <input type="text" id="updateInput" onChange={(e) => {
+  //           setNewName(e.target.value)
+  //         }}/>
+  //       <button onClick={ () => [updateUser(val.username)]}
+  //       >Update</button>
+
+  //     </div>
+  //   );
+  // }
+  // )}
   
   return (
     <div className='container'>
@@ -105,29 +127,6 @@ function SignUp() {
         />
       </InputGroup>
       <Button onClick={submitUser} variant="primary">Submit</Button>
-
-      {userList.map((val) => {
-        return (
-          <div className='card'>
-            <h1>{val.email}</h1>
-            <p>{val.password}</p>
-
-            <button onClick={() => {deleteUser(val.username);
-            }}
-            >
-              Delete
-              </button>
-              <input type="text" id="updateInput" onChange={(e) => {
-                setNewName(e.target.value)
-              }}/>
-            <button onClick={ () => [updateUser(val.username)]}
-            >Update</button>
-
-          </div>
-        );
-      }
-      
-      )}
     </>
     </div>
   );
