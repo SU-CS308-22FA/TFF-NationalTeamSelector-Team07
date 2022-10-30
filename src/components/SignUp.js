b3026c42d7de27cd1db8fa7f48040cd4090137c6
Mustafa Ata Onbas <aonbas@sabanciuniv.edu>
2022-10-30 19:25:33 +0300

    SignUp inputs added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,134 +1,108 @@
 import './SignUp.css'
-import {Form, InputGroup, Button} from 'react-bootstrap';
-import {useState} from 'react'
-import Axios from 'axios';
+import {Form, Button} from 'react-bootstrap';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 function SignUp() {
 
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+ const [form, setForm] = useState({
+   name: "",
+   surname: "",
+   email:"",
+   username:"",
+   password:""
+ });
+ 
+ const navigate = useNavigate();
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+ 
+   // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPerson = { ...form };
+ 
+   await fetch("http://localhost:5500/record/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+ 
+   setForm({ name: "", surname: "", email: "", username:"", password:"" });
+   navigate("/UserSettings");
+   console.log(form.name,form.email,form.password,form.username, form.surname)
+  }
 
-  // const [userList, setUserList] = useState([]); //here adding feature direct adding to list of map. 4-5 minutes
-  // const [newName, setNewName] = useState("");
-
-  // useEffect(() => { //burası get ile db'den alınacaklar için kullanılıyor. useEffect !!
-  //   Axios.get("http://localhost:3001/api/get").then((response) => {
-  //     console.log(response.data);
-  //     setUserList(response.data)
-  //   })
-  // }, []);
-
-  // const submitUser = () => { //db'ye insert etmek için kullanılıyor
-  //   Axios.post("http://localhost:5432/api/insert", {
-  //     name: name, 
-  //     surname: surname,
-  //     email: email,
-  //     password: password,
-  //     username: username
-  //   }).then(() => {
-  //     alert("succesfull insert");
-  //   });
-  // };
-
-  // const deleteUser = (user) => {
-  //   Axios.delete('http://localhost:3001/api/delete/${user}' )
-  // }
-
-  // const updateUser = (username) => {
-  //   Axios.put("http://localhost:3001/api/update", {
-  //     name: name,  //BURALAR KARIŞTI
-  //     surname: surname,
-  //     email: email,
-  //     password: password,
-  //     username: username
-  //   });
-  //   setNewName("")
-  // }
-
-  // {userList.map((val) => {
-  //   return (
-  //     <div className='card'>
-  //       <h1>{val.email}</h1>
-  //       <p>{val.password}</p>
-
-  //       <button onClick={() => {deleteUser(val.username);
-  //       }}
-  //       >
-  //         Delete
-  //         </button>
-  //         <input type="text" id="updateInput" onChange={(e) => {
-  //           setNewName(e.target.value)
-  //         }}/>
-  //       <button onClick={ () => [updateUser(val.username)]}
-  //       >Update</button>
-
-  //     </div>
-  //   );
-  // }
-  // )}
-  
   return (
     <div className='container'>
       <h3 className='textLayout' style={{marginTop:"80px"}}>Sign Up</h3>
-      <>
-      <Form.Label style={{marginTop:"80px"}}>Personal Information</Form.Label>
-      <InputGroup className="mb-3" onChange={(e) => {
-        setName(e.target.value)
-      }}>
-        <Form.Control
-          placeholder="Please enter your name"
-          aria-label="name"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-
-      <InputGroup className="mb-3" onChange={(e) => {
-        setSurname(e.target.value)
-      }}>
-        <Form.Control
-          placeholder="Please enter your surname"
-          aria-label="surname"
-          aria-describedby="basic-addon2"
-        />
-      </InputGroup>
-
-      <br/>
+      <Form.Label style={{marginTop:"60px"}}>Personal Information</Form.Label>
+      <form style={{marginBottom:"10px"}}>
+         <input
+           type="text"
+           className="form-control"
+           id="name"
+           value={form.name}
+           placeholder="Please enter your name"
+           onChange={(e) => updateForm({ name: e.target.value })}
+         />
+      </form>
+      <form style={{marginBottom:"30px"}}>
+         <input
+           type="text"
+           className="form-control"
+           id="surname"
+           value={form.surname}
+           placeholder="Please enter your surname"
+           onChange={(e) => updateForm({ surname: e.target.value })}
+         />
+      </form>
 
       <Form.Label>Account Information</Form.Label>
-      <InputGroup className="mb-3" onChange={(e) => {
-        setEmail(e.target.value)
-      }}>
-      <Form.Control
-          placeholder="Please enter your e-mail address"
-          aria-label="email"
-          aria-describedby="basic-addon2"
-        />
-      </InputGroup>
-
-      <InputGroup className="mb-3" onChange={(e) => {
-        setUsername(e.target.value)
-      }}>
-      <Form.Control
-          placeholder="Please enter your username"
-          aria-label="username"
-          aria-describedby="basic-addon2"
-        />
-      </InputGroup>
-
-      <InputGroup className="mb-3" onChange={(e) => {
-        setPassword(e.target.value)
-      }}>
-      <Form.Control
-          placeholder="Please enter your password"
-          aria-label="password"
-          aria-describedby="basic-addon2"
-        />
-      </InputGroup>
-      <Button variant="primary">Submit</Button>
-    </>
+      <form style={{marginBottom:"10px"}}>
+         <input
+           type="text"
+           className="form-control"
+           id="email"
+           value={form.email}
+           placeholder="Please enter your email"
+           onChange={(e) => updateForm({ email: e.target.value })}
+         />
+      </form>
+      <form style={{marginBottom:"10px"}}>
+         <input
+           type="text"
+           className="form-control"
+           id="username"
+           value={form.username}
+           placeholder="Please enter your username"
+           onChange={(e) => updateForm({ username: e.target.value })}
+         />
+      </form>
+      <form style={{marginBottom:"30px"}}>
+         <input
+           type="text"
+           className="form-control"
+           id="password"
+           value={form.password}
+           placeholder="Please enter your password"
+           onChange={(e) => updateForm({ password: e.target.value })}
+         />
+      </form>
+      <Button onClick={onSubmit} variant="primary">Submit</Button>
     </div>
   );
 }
