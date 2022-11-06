40bc0d100cab73b7b977cb6baf761930b7bbefca
Mustafa Ata Onbas <aonbas@sabanciuniv.edu>
2022-11-06 23:09:50 +0300

    heroku deploy fixes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv')
@@ -22,5 +23,22 @@ app.get('/', (req,res) => {
 //routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use(errorHandler)
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+  } else {
+    app.get('/', (_, res) => {
+      res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+  }
+  
+  app.use(errorHandler)
 
 app.listen(8080, () => console.log(`server started on port 8080`))
