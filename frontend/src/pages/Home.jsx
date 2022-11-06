15e5caf6ed6cfee333628fd1c4b8938061063f49
bpolat_CS <bpolat@sabanciuniv.edu>
2022-11-06 22:56:19 +0300

    style changings

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,15 +6,10 @@ function Home() {
     return (
         <>
             <section className="heading">
-                <h1>What do you need help with?
-                    <p>Please choose from an option below</p>
+                <h1>TFF National Football Team Selector 
+                    <p>Develpment in progress...</p>
                 </h1>
             </section>
-
-            <Link to='/new-ticket' className='btn btn-reverse 
-            btn-block'> 
-                <FaQuestionCircle /> Do you have any questions?
-            </Link>
             <Link to='/profile' className='btn
             btn-block'> 
                 <FaTicketAlt /> View My Profile
