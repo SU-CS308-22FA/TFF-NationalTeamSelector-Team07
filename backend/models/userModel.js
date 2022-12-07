827b3ca9cbf66ab36b29abf5717daf0d77939866
bengisuozdemir <bengisuozdemir@sabanciuniv.edu>
2022-12-07 17:43:55 +0300

    profile page finished

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,6 +20,12 @@ const userSchema = mongoose.Schema({
         type: Boolean,
         required: true,
         default: false
+    }, 
+    verification: {
+        type: Boolean,
+        required: true,
+        default: false
+        // false= normal user
     }
 },
 {
