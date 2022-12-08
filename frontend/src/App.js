d6c3f46d024ab454d3d770f55b8365a19c9587f1
bpolat_CS <bpolat@sabanciuniv.edu>
2022-12-08 18:53:41 +0300

    player profile implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import Player from './pages/Player'
 import EditPlayer from './pages/EditPlayer'
 import ViewAllPlayers from './pages/ViewAllPlayers'
 import DisplayTeam from './pages/DisplayUserTeams'
+import PlayerProfilePage from './pages/PlayerProfilePage'
 
 function App() {
   return(
@@ -46,13 +47,14 @@ function App() {
             <Route path='/new-team' element={<NewTeam />}/>
 
             <Route path='/new-player' element={<NewPlayer />}/>
-            
+
+            <Route path='/player-profile' element={<PlayerProfilePage />}/>
 
             <Route path='/teams' element={<PrivateRoute />}>
-              <Route path='/teams' element={<Teams />}/>
+            <Route path='/teams' element={<Teams />}/>
             </Route>
             <Route path='/team/:teamId' element={<PrivateRoute />}>
-              <Route path='/team/:teamId' element={<Team />}/>
+            <Route path='/team/:teamId' element={<Team />}/>
             </Route>
         </Routes>
       </div>
