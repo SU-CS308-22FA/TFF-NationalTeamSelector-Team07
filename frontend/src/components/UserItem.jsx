7bca6432076d819a7c61c22e5fc8bb8037f30bfd
bengisuozdemir <bengisuozdemir@sabanciuniv.edu>
2022-12-12 21:30:45 +0300

    bug fix

--- frontend/src/components/UserItem.jsx
+++ frontend/src/components/UserItem.jsx
@@ -10,7 +10,7 @@ function UserItem({user}) {
     
     const dispatch = useDispatch()
     // const {user} = useSelector( (state) => state.auth)
-    console.log("userItem"  + JSON.stringify(user))
+    //console.log("userItem"  + JSON.stringify(user))
     const [userID] = useState(user._id)
     const [username] = useState(user.name)
     const [email] = useState(user.email)
