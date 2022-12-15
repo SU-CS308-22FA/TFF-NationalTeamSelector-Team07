7f1d3788c2f6f3c553e558295ea881ab5df22df7
bengisuozdemir <bengisuozdemir@sabanciuniv.edu>
2022-12-15 15:21:19 +0300

    new player dropdown progress

--- frontend/src/pages/NewTeam.jsx
+++ frontend/src/pages/NewTeam.jsx
@@ -12,11 +12,11 @@ import playerService from '../features/players/playerService'
 
 function NewTeam() {
     const { user } = useSelector((state) => state.auth)
-    const { players, player } = useSelector((state) => state.players)
+    //const { players, player } = useSelector((state) => state.players)
     const [playerList, setPlayerList] = useState([])
     const {isLoading, isError, isSuccess, message} = useSelector((state) => state.teams)
-    const [playerName] = useState(players.fullName)
-    const [playerPosition] = useState(players.position)
+    //const [playerName] = useState(players.fullName)
+    //const [playerPosition] = useState(players.position)
 
     const [player1, setPlayerName1] = useState("")
     const [player2, setPlayerName2] = useState("")
@@ -51,12 +51,11 @@ function NewTeam() {
         // else{
         //     toast.error('please select a different player')
         // }
-        //dispatch(createTeam({player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, teamName, email}))
+        dispatch(createTeam({player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, teamName, email}))
         }
     
     const playerOptions = playerList.map((player, index) => (
-        
-        <option key={player._id} value={player.value} defaultValue="">{player.fullName}</option>
+    <PlayerDropdownItem key={index} player={player} >{player.fullName}</PlayerDropdownItem>
     ))
     
 
@@ -84,7 +83,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player1">Player1 - Goalkeeper: {player1}</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player1} onChange={(e) => setPlayerName1(e.value.fullName)} key={Math.random()}  placeholder="Select a Goalkeeper" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player1} onChange={(e) => setPlayerName2(player1)} key={Math.random()}  placeholder="Select a Goalkeeper" />
                     </form>
                      
                     {/* <input type="text" 
@@ -95,7 +94,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player2">Player2 - Defender</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player2} onChange={(e) => setPlayerName2(e.value.fullName)} key={Math.random()} placeholder="Select a Defender" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player2} onChange={(e) => setPlayerName2(player2)} key={Math.random()} placeholder="Select a Defender" />
                     </form>
                    {/* <input type="text" 
                     className="form-control"
@@ -105,7 +104,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player3">Player3 - Defender</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player3} onChange={(e) => setPlayerName3(e.value.fullName)} key={Math.random()} placeholder="Select a Defender" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player3} onChange={(e) => setPlayerName3(player3)} key={Math.random()} placeholder="Select a Defender" />
                     </form>
                    {/* <input type="text" 
                     className="form-control"
@@ -115,7 +114,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player4">Player4 - Defender</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player4} onChange={(e) => setPlayerName4(e.value.fullName)} key={Math.random()} placeholder="Select a Defender" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player4} onChange={(e) => setPlayerName4(player4)} key={Math.random()} placeholder="Select a Defender" />
                     </form>
                     {/*<input type="text" 
                     className="form-control"
@@ -125,7 +124,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player5">Player5 - Defender</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player5} onChange={(e) => setPlayerName5(e.value.fullName)} key={Math.random()} placeholder="Select a Defender" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player5} onChange={(e) => setPlayerName5(player5)} key={Math.random()} placeholder="Select a Defender" />
                     </form>
                     {/*<input type="text" 
                     className="form-control"
@@ -135,7 +134,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player6 - Midfielder</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player6} onChange={(e) => setPlayerName6(e.value.fullName)} key={Math.random()} placeholder="Select a Midfielder" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player6} onChange={(e) => setPlayerName6(player6)} key={Math.random()} placeholder="Select a Midfielder" />
                     </form>
                     {/*<input type="text" 
                     className="form-control"
@@ -145,7 +144,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player7 - Midfielder</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player7} onChange={(e) => setPlayerName7(e.value.fullName)} key={Math.random()} placeholder="Select a Midfielder" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player7} onChange={(e) => setPlayerName7(player7)} key={Math.random()} placeholder="Select a Midfielder" />
                     </form>
                     {/* <input type="text" 
                     className="form-control"
@@ -155,7 +154,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player8 - Midfielder</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player8} onChange={(e) => setPlayerName8(e.value.fullName)} key={Math.random()} placeholder="Select a Midfielder" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player8} onChange={(e) => setPlayerName8(player8)} key={Math.random()} placeholder="Select a Midfielder" />
                     </form>
                     {/* <input type="text" 
                     className="form-control"
@@ -165,7 +164,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player9 - Midfielder</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player9} onChange={(e) => setPlayerName9(e.value.fullName)} key={Math.random()} placeholder="Select a Midfielder" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player9} onChange={(e) => setPlayerName9(player9)} key={Math.random()} placeholder="Select a Midfielder" />
                     </form>
                     {/* <input type="text" 
                     className="form-control"
@@ -175,7 +174,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player10 - Forward</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player10} onChange={(e) => setPlayerName10(e.value.fullName)} key={Math.random()} placeholder="Select a Forward" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player10} onChange={(e) => setPlayerName10(player10)} key={Math.random()} placeholder="Select a Forward" />
                     </form>
                     {/* <input type="text" 
                     className="form-control"
@@ -185,7 +184,7 @@ function NewTeam() {
                 <div className="form-group">
                     <label htmlFor="player">Player11 - Forward</label>
                     <form type="dropdown">
-                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player11} onChange={(e) => setPlayerName11(e.value.fullName)} key={Math.random()} placeholder="Select a Forward" />
+                        <Dropdown required={true} id="dropdown-basic-button" options={playerOptions} value={player11} onChange={(e) => setPlayerName11(player11)} key={Math.random()} placeholder="Select a Forward" />
                     </form>
                     {/* <input type="text" 
                     className="form-control"
