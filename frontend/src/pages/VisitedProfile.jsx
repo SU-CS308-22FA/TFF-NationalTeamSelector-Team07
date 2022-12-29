a4e45d124a1503bc7e8f6ba05b0c1d0840889218
esra nur özüm <esraozum@sabanciuniv.edu>
2022-12-29 16:50:34 +0300

    bug fix

--- frontend/src/pages/VisitedProfile.jsx
+++ frontend/src/pages/VisitedProfile.jsx
@@ -78,93 +78,102 @@ function VisitedProfile() {
                             </MDBListGroup>
                         </MDBCardBody>
                     </MDBCard>
-      {/* ///////////////////////////////////////////comment part */}
-      <MDBContainer className="card-body p-0" style={{marginTop:'30px'}}>
-      <MDBRow>
-        <MDBCol style={{width:'100%'}}>
-          <MDBCard
-            className="shadow-0 border"
-            style={{ backgroundColor: 'white' }}
-          >
-            <MDBCardBody>
-              <MDBInput wrapperClass="mb-4" placeholder="Type comment..." label={"Comment to " +location.state.name} />
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
+                {/* ///////////////////////////////////////////comment part */}
+                <MDBContainer className="card-body p-0" style={{marginTop:'30px'}}>
+                <MDBRow>
+                    <MDBCol style={{width:'100%'}}>
+                        <MDBCard
+                            className="shadow-0 border"
+                            style={{ backgroundColor: 'white' }}
+                        >
+                            <MDBCardBody>
+                            <MDBInput wrapperClass="mb-4" placeholder="Type comment..." label={"Comment to " +location.state.name} />
+                            <MDBCard className="mb-4">
+                                <MDBCardBody>
+                                <p>Type your note, and hit enter to add it</p>
 
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Martha</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center">
-                      <p className="small text-muted mb-0">Upvote?</p>
-                      <MDBIcon
-                        far
-                        icon="thumbs-up mx-2 fa-xs text-black"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small text-muted mb-0">3</p>
-                    </div>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-row align-items-center">
+                                    <MDBCardImage
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp"
+                                        alt="avatar"
+                                        width="25"
+                                        height="25"
+                                    />
+                                    <p className="small mb-0 ms-2">Martha</p>
+                                    </div>
+                                    <div className="d-flex flex-row align-items-center">
+                                    <p className="small text-muted mb-0">Upvote?</p>
+                                    <MDBIcon
+                                        far
+                                        icon="thumbs-up mx-2 fa-xs text-black"
+                                        style={{ marginTop: "-0.16rem" }}
+                                    />
+                                    <p className="small text-muted mb-0">3</p>
+                                    </div>
+                                </div>
+                                </MDBCardBody>
+                            </MDBCard>
+                            <MDBCard className="mb-4">
+                                <MDBCardBody>
+                                <p>Type your note, and hit enter to add it</p>
 
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Mary Kate</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center text-primary">
-                      <p className="small mb-0">Upvoted</p>
-                      <MDBIcon
-                        fas
-                        icon="thumbs-up mx-2 fa-xs"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small mb-0">2</p>
-                    </div>
-                    <MDBRow>
-                        <MDBCol lg="12">
-                        <div className="tickets">
-                            <div className="ticket-headings">
-                               <div></div>
-                               <div></div>
-                               <div></div>
-                                <form onSubmit={reportUser}  >
-                                    <button className="btn btn-reverse btn-sm"> <FaBiohazard></FaBiohazard> Report</button>
-                                </form>
-                                <style>
-                                    {`
-                                        .like-button {
-                                        font-size: 1rem;
-                                            padding: 9px 10px;
-                                            color:  #585858;
-                                        }
-                                        .liked {
-                                            font-weight: bold;
-                                            color: #1565c0;
-                                        }
-                                    `}
-                                </style>      
-                            </div>
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-row align-items-center">
+                                    <MDBCardImage
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp"
+                                        alt="avatar"
+                                        width="25"
+                                        height="25"
+                                    />
+                                    <p className="small mb-0 ms-2">Mary Kate</p>
+                                    </div>
+                                    <div className="d-flex flex-row align-items-center text-primary">
+                                    <p className="small mb-0">Upvoted</p>
+                                    <MDBIcon
+                                        fas
+                                        icon="thumbs-up mx-2 fa-xs"
+                                        style={{ marginTop: "-0.16rem" }}
+                                    />
+                                    <p className="small mb-0">2</p>
+                                    </div>
+                                </div>
+                                </MDBCardBody>
+                            </MDBCard>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+                </MDBContainer>
+    
+                <MDBRow>
+                    <MDBCol lg="12">
+                    <div className="tickets">
+                        <div className="ticket-headings">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <form onSubmit={reportUser}  >
+                                <button className="btn btn-reverse btn-sm"> <FaBiohazard></FaBiohazard> Report</button>
+                            </form>
+                            <style>
+                                {`
+                                    .like-button {
+                                    font-size: 1rem;
+                                        padding: 9px 10px;
+                                        color:  #585858;
+                                    }
+                                    .liked {
+                                        font-weight: bold;
+                                        color: #1565c0;
+                                    }
+                                `}
+                            </style>      
                         </div>
-                        </MDBCol>
+                    </div>
+                    </MDBCol>
                         
-                    </MDBRow>
+                </MDBRow>
                 </MDBCol>
                 <MDBCol lg="8">
                     <MDBCard className="mb-4">
