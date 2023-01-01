ca2dda674d106eb66e65a45e2af86ae74dbae258
bpolat_CS <bpolat@sabanciuniv.edu>
2023-01-01 16:42:44 +0300

    hisotric player view added to home page

--- frontend/src/components/MainPagePlayerItem.jsx
+++ frontend/src/components/MainPagePlayerItem.jsx
@@ -1,12 +1,18 @@
-
 import {useState} from 'react'
-import { useDispatch } from 'react-redux'
+import {toast} from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { deletePlayer } from '../features/players/playerSlice'
 import {useNavigate} from 'react-router-dom'
+import {getHistoric} from '../features/historics/historicSlice'
+import { useEffect } from 'react'
+
 
 
 
 function MainPagePlayerItem({player}) {
-    
+    const dispatch = useDispatch()
+
     const [FullName] = useState(player.fullName)
     const [Team] = useState(player.team)
     const [player_id] = useState(player._id)
@@ -16,7 +22,7 @@ function MainPagePlayerItem({player}) {
     const [PreferedFoot] = useState(player.PreferedFoot)
     const [Age] = useState(player.Age)
     const [PlaceOfBirth] = useState(player.PlaceOfBirth)
-
+    const [personel] = useState(player.personel)
     const navigate = useNavigate()
 
     const playerData = {
@@ -31,18 +37,23 @@ function MainPagePlayerItem({player}) {
         pob: PlaceOfBirth
     }
 
+    useEffect(() => {
+        return () => {
+            dispatch(getHistoric(personel))
+        }
+    }, [dispatch])
     
-  
+
     const handleViewPlayerInfo = (e) => {
         e.preventDefault()
         //console.log(player_id)
-        console.log("24 line: " + DateOfBirth)
-        navigate('/player-profile', {state: {personel: player_id, name: FullName, 
+        //console.log("24 line: " + DateOfBirth)
+        navigate('/player-profile/:personel', {state: {personel: personel, playerID: player_id, name: FullName, 
             team: Team, pos: Position, Rating: Rating, dob: DateOfBirth, 
             foot: PreferedFoot, age: Age, pob: PlaceOfBirth} })
         //window.location.reload()
     }
-
+    
     return (
         <div className="tickets">
             <div className="ticket-headings">
