963d801139885c278a44602e0e26f8ea5650f053
esra nur özüm <esraozum@sabanciuniv.edu>
2023-01-02 21:39:36 +0300

    add like button test case

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_cases_for_app():
     driver = webdriver.Chrome()
 
+########## REGISTER TEST ##############
+
     # Test successful registration
     driver.get('http://localhost:3000/register')
     email_field = driver.find_element_by_name('name')
@@ -35,10 +37,13 @@ def test_cases_for_app():
     error_message = driver.find_element(By.CSS_SELECTOR, '.error').text
     assert error_message == 'Passwords do not match'
 
+########### LOAD HOMEPAGE TEST ############
+
     # Test the home page
     driver.get('http://localhost:3000/')
     assert driver.title == 'My MERN App'
 
+########## LOGIN TEST #############
 
     # Test the failed login form
     driver.get('http://localhost:3000/login')
@@ -61,5 +66,27 @@ def test_cases_for_app():
     driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
     error_message = driver.find_element(By.CSS_SELECTOR, '.error').text
     assert error_message == 'credentials are not correct'
+
+
+############ LIKE BUTTON TEST ############
+
+    # Get the initial like count
+    like_count_element = driver.find_element(By.CSS_SELECTOR, '.like-count')
+    like_count = int(like_count_element.text)
+
+    # Click the like button
+    driver.find_element(By.CSS_SELECTOR, '.like').click()
+
+    # Wait for the page to reload
+    WebDriverWait(driver, 10).until(
+        EC.staleness_of(like_count_element)
+    )
+
+    # Get the updated like count
+    like_count_element = driver.find_element(By.CSS_SELECTOR, '.like-count')
+    updated_like_count = int(like_count_element.text)
+
+    # Verify that the like count has incremented by one
+    assert updated_like_count == like_count + 1
 
     driver.quit()
